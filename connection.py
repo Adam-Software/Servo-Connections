9790abaa8b94551eb1101138b9e0714f902778ba
Alexey Nesterov <vertigra.developer@gmail.com>
2022-09-08 12:48:53 +1000

    Create connection.py

---
+++ connection.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+from scservo_sdk import *
+import logging
+
+class MetaSingleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class Connection(metaclass=MetaSingleton):
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', encoding='utf-8', level=logging.DEBUG)
+
+    _BAUDRATE = 1000000
+    _DEVICENAME = '/dev/ttyUSB0'
+    _portHandler = None
+    _packetHandler = None
+
+    def _initPortHandler(self):
+        if self._portHandler is None:
+            self._portHandler = PortHandler(self._DEVICENAME)
+            self._openPort()
+
+    def _initPacketHandler(self):
+        if self._packetHandler is None:
+            self._packetHandler = sms_sts(self._portHandler)
+            self._setBaudrate()
+
+    def _setBaudrate(self):
+        if self._portHandler.setBaudRate(self._BAUDRATE):
+            logging.info("Succeeded to change the baudrate")
+
+    def _openPort(self):
+        if self._portHandler.openPort():
+            logging.info("Succeeded to open the port")
+
+    def getPacketHandler(self):
+        if self._packetHandler is None:
+            self._initPortHandler()
+            self._initPacketHandler()
+
+        return self._packetHandler
