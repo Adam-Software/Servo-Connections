141923d44850648d72cc2cdb4a13166b8d71d920
Alexey Nesterov <vertigra.developer@gmail.com>
2022-09-09 13:16:56 +1000

    Change: experement with test_close_port

--- UnitTest.py
+++ UnitTest.py
@@ -4,19 +4,14 @@ from servo_serial.connection import Connection
 
 class MyTestCase(unittest.TestCase):
 
-    def similarity_test_of_class_instances(self):
+    def test_similarity_class_instances(self):
         connect1 = Connection()
         connect2 = Connection()
         self.assertEqual(connect1, connect2)
 
-    def test_close_port(self):
+    def test_not_init_close_port(self):
         connect = Connection()
-        port_handler = connect.getPortHandler()
-        packet_handler = connect.getPacketHandler()
-
-        connect.closePort()
-        self.assertEqual(port_handler, None)
-        self.assertEqual(packet_handler, None)
+        self.assertEqual(connect.closePort(), None)
 
 
 if __name__ == '__main__':
