53e837a59f5f7c1e7258a3ef6f14874dad34001a
Alexey Nesterov <vertigra.developer@gmail.com>
2022-09-09 15:19:07 +1000

    added: test for change log level

--- connectTestCase.py
+++ connectTestCase.py
@@ -5,24 +5,31 @@ from servo_serial.connection import Connection
 
 class ConnectTestCase(unittest.TestCase):
 
-    def test_similarity_class_instances(self):
+    def testSimilarityClassInstances(self):
         connect1 = Connection()
         connect2 = Connection()
         self.assertEqual(connect1, connect2)
 
-    def test_not_init_close_port(self):
+    def testNotInitClosePort(self):
         connect = Connection()
         self.assertEqual(connect.closePort(), False)
 
-    def test_init_close_port(self):
+    def testInitClosePort(self):
         connect = Connection()
         connect.getPacketHandler()
         self.assertEqual(connect.closePort(), True)
 
-    def test_default_loglevel(self):
+    def testDefaultLoglevel(self):
         connect = Connection()
         level = connect.getLoggingLevel()
         self.assertEqual(level, logging.ERROR)
+
+    def testChangeLoglevel(self):
+        connect = Connection()
+        targetLevel = logging.INFO
+        connect.setLogginLevel(targetLevel)
+        self.assertEqual(connect.getLoggingLevel(), targetLevel)
+
 
 
 if __name__ == '__main__':
