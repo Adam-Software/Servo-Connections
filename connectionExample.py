03be95c15a00ac19257b5a4bd9fee13fdd98db0c
Alexey Nesterov <vertigra.developer@gmail.com>
2022-10-18 10:27:09 +1000

    Added: update example for sdk v.0.2.0

--- connectionExample.py
+++ connectionExample.py
@@ -6,29 +6,29 @@ from scservo_sdk import *
 
 class ConnectionExample():
 
-    SCSCL_PRESENT_VOLTAGE      = 62
-    SCS_ID                     = 13
-    scs_end                    = 0
+    SCSCL_PRESENT_VOLTAGE = 62
+    SCS_ID = 13
 
     packetHandler = Connection().getPacketHandler()
+    portHandler = Connection.getPortHandler()
 
     def getVoltage(self):
-        scs_present_voltage_speed, scs_comm_result, scs_error = self.packetHandler.read4ByteTxRx(self.SCS_ID, self.SCSCL_PRESENT_VOLTAGE)
+
+        scs_present_voltage_speed, scs_comm_result, scs_error = packetHandler.read4ByteTxRx(portHandler, SCS_ID,
+                                                                                            SCSCL_PRESENT_VOLTAGE)
         print(scs_present_voltage_speed)
 
         if scs_comm_result != COMM_SUCCESS:
-           print(self.packetHandler.getTxRxResult(scs_comm_result))
+            print(packetHandler.getTxRxResult(scs_comm_result))
 
         elif scs_error != 0:
-            print(self.packetHandler.getRxPacketError(scs_error))
+            print(packetHandler.getRxPacketError(scs_error))
 
-        scs_present_voltage = protocol_packet_handler.scs_makeword(self, scs_present_voltage_speed, scs_comm_result)
-        print(scs_present_voltage)
-
-        percent_voltage = ((scs_present_voltage - 92)/(125 - 92))*100
+        scs_present_voltage = SCS_MAKEWORD(scs_present_voltage_speed, scs_comm_result)
+        percent_voltage = ((scs_present_voltage - 92) / (125 - 92)) * 100
 
         intvol = int(percent_voltage)
-        return intvol
+        print(intvol)
 
 
 voltage = ConnectionExample().getVoltage()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='servo_serial',
-    version='0.0.1',
+    version='0.0.2',
     packages=['servo_serial'],
     url='https://github.com/Adam-Software/Servo-serial',
     license='MIT',
